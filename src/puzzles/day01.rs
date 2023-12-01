f541aeb8f0bd020b3bd357e22db166bd596a860e
Nathan Plamondon <nplamondon@gmail.com>
2023-12-01 12:05:26 -0700

    d01: Solve part 2

--- src/puzzles/day01.rs
+++ src/puzzles/day01.rs
@@ -1,25 +1,25 @@
 use crate::RunError;
 
-pub fn main(part: u8, data: &str) -> Result<usize, RunError> {
+pub fn main(part: u8, data: &String) -> Result<usize, RunError> {
     let parsed_data = parse_data(data)?;
 
     match part {
-        1 => part1(&parsed_data),
-        2 => part2(&parsed_data),
+        1 => part1(parsed_data),
+        2 => part2(parsed_data),
         _ => Err(RunError::BadPartNum),
     }
 }
 
-fn parse_data(data: &str) -> Result<Vec<&str>, RunError> {
+fn parse_data(data: &String) -> Result<Vec<String>, RunError> {
     let lines: Vec<&str> = data[..].split('\n').collect();
 
-    match lines.iter().map(|x| Ok(x.trim())).collect() {
+    match lines.iter().map(|x| Ok(x.trim().to_string())).collect() {
         Ok(parsed_data) => Ok(parsed_data),
         Err(e) => Err(RunError::ParseString(e)),
     }
 }
 
-fn part1(values: &[&str]) -> Result<usize, RunError> {
+fn part1(values: Vec<String>) -> Result<usize, RunError> {
     // Goal: Concatenate first and last digit for each line. Sum all lines.
 
     let mut total = 0;
@@ -43,10 +43,25 @@ fn part1(values: &[&str]) -> Result<usize, RunError> {
     Ok(total.try_into().unwrap())
 }
 
-fn part2(values: &[&str]) -> Result<usize, RunError> {
-    // What's the goal?
+fn part2(values: Vec<String>) -> Result<usize, RunError> {
+    // Same as part 1, but spelled-out digits count, too.
 
-    todo!();
+    let mut parsed_values: Vec<String> = vec![];
+    for line in values {
+        let new_line = line.replace("one", "o1e")
+        .replace("two", "t2o")
+        .replace("three", "t3e")
+        .replace("four", "4")
+        .replace("five", "5e")
+        .replace("six", "6")
+        .replace("seven", "7n")
+        .replace("eight", "e8t")
+        .replace("nine", "n9e");
+
+        parsed_values.push(new_line);
+    }
+
+    part1(parsed_values)
 }
 
 fn maybe_convert_char_to_digit(character: char) -> Option<u32> {
@@ -61,21 +76,40 @@ mod tests {
     pqr3stu8vwx
     a1b2c3d4e5f
     treb7uchet";
-    static SAMPLE_DATA: &'static [&str] = &["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
-    static SAMPLE_GOALS: [usize; 2] = [142, 0];
+
+    static SAMPLE_DATA: &[&str] = &[
+        "1abc2",
+        "pqr3stu8vwx",
+        "a1b2c3d4e5f",
+        "treb7uchet"];
+
+    static SAMPLE_DATA_2: &[&str] = &[
+        "two1nine",
+        "eightwothree",
+        "abcone2threexyz",
+        "xtwone3four",
+        "4nineeightseven2",
+        "zoneight234",
+        "7pqrstsixteen"];
+
+    static SAMPLE_GOALS: [usize; 2] = [142, 281];
 
     #[test]
     fn test_parse() {
-        assert_eq!(parse_data(&SAMPLE_INPUT).unwrap(), SAMPLE_DATA);
+        assert_eq!(parse_data(&SAMPLE_INPUT.to_string()).unwrap(), SAMPLE_DATA);
     }
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(&SAMPLE_DATA).unwrap(), SAMPLE_GOALS[0]);
+        let mut sample_data: Vec<String> = vec![];
+        SAMPLE_DATA.iter().for_each(|line| sample_data.push((*line).to_string()));
+        assert_eq!(part1(sample_data).unwrap(), SAMPLE_GOALS[0]);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(&SAMPLE_DATA).unwrap(), SAMPLE_GOALS[1]);
+        let mut sample_data: Vec<String> = vec![];
+        SAMPLE_DATA_2.iter().for_each(|line| sample_data.push((*line).to_string()));
+        assert_eq!(part2(sample_data).unwrap(), SAMPLE_GOALS[1]);
     }
 }
